42b6f660a892bd75434ac2dabd612f1d432e4d8e
Valres <tsterle59@gmail.com>
2024-05-07 23:45:22 +0200

    Updated main class

--- src/Valres/Skyblock/Skyblock.php
+++ src/Valres/Skyblock/Skyblock.php
@@ -4,13 +4,30 @@ namespace Valres\Skyblock;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
+use Valres\Skyblock\libs\poggit\libasynql\DataConnector;
+use Valres\Skyblock\libs\poggit\libasynql\libasynql;
+use Valres\Skyblock\player\PlayerManager;
+use Valres\Skyblock\skyblock\SkyblockManager;
 
 class Skyblock extends PluginBase
 {
     use SingletonTrait;
 
-    protected function onEnable(): void {
+    private PlayerManager $playerManager;
+    private SkyblockManager $skyblockManager;
 
+    private DataConnector $database;
+
+    protected function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->saveResource("mysql.sql");
+        $this->database = libasynql::create($this, $this->getConfig()->get("database"), [
+            "sqlite" => "sqlite.sql",
+            "mysql" => "mysql.sql"
+        ]);
+
+        $this->database->executeGeneric("players.init");
+        $this->database->executeGeneric("skyblocks.init");
     }
 
     protected function onLoad(): void {
@@ -19,5 +36,17 @@ class Skyblock extends PluginBase
 
     protected function onDisable(): void {
 
+    }
+
+    public function getPlayerManager(): PlayerManager {
+        return $this->playerManager;
+    }
+
+    public function getSkyblockManager(): SkyblockManager {
+        return $this->skyblockManager;
+    }
+
+    public function getDatabase(): DataConnector {
+        return $this->database;
     }
 }
