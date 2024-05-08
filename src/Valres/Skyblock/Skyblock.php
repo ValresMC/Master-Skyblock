465127182b72d574b770891bb9e3db92bf359e09
Valres <tsterle59@gmail.com>
2024-05-08 19:45:03 +0200

    Update Skyblock.php

--- src/Valres/Skyblock/Skyblock.php
+++ src/Valres/Skyblock/Skyblock.php
@@ -7,6 +7,8 @@ use pocketmine\utils\SingletonTrait;
 use pocketmine\world\generator\GeneratorManager;
 use Valres\Skyblock\commands\IslandCommand;
 use Valres\Skyblock\generator\Island;
+use Valres\Skyblock\libs\CortexPE\Commando\exception\HookAlreadyRegistered;
+use Valres\Skyblock\libs\CortexPE\Commando\PacketHooker;
 use Valres\Skyblock\libs\poggit\libasynql\DataConnector;
 use Valres\Skyblock\libs\poggit\libasynql\libasynql;
 use Valres\Skyblock\listeners\player\PlayerJoin;
@@ -22,8 +24,12 @@ class Skyblock extends PluginBase
 
     private DataConnector $database;
 
+    /**
+     * @throws HookAlreadyRegistered
+     */
     protected function onEnable(): void {
         $this->saveDefaultConfig();
+        @mkdir($this->getDataFolder() . "island/");
 
         $this->database = libasynql::create($this, $this->getConfig()->get("database"), [
             "sqlite" => "sqlite.sql",
@@ -41,9 +47,10 @@ class Skyblock extends PluginBase
 
         $this->getServer()->getPluginManager()->registerEvents(new PlayerJoin(), $this);
 
-        $this->getServer()->getCommandMap()->registerAll("master-skyblock", [new IslandCommand()]);
-
-        GeneratorManager::getInstance()->addGenerator(Island::class, "island", fn () => null);
+        if(!PacketHooker::isRegistered()) PacketHooker::register($this);
+        $this->getServer()->getCommandMap()->registerAll("master-skyblock", [
+            new IslandCommand($this, "island", "Island base commands", ["is", "skyblock"])
+        ]);
     }
 
     protected function onLoad(): void {
